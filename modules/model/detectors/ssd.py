3124f53ffcbfa0d78b9ffc9039e944be8653a0df
IamJiyong <din2346@gmail.com>
2024-08-01 14:32:43 +0000

    Fixed bug

--- modules/model/detectors/ssd.py
+++ modules/model/detectors/ssd.py
@@ -1,6 +1,9 @@
 from modules.model.detectors.detector_template import DetectorTemplate
-
+from modules.model.layers.backbone.vgg_backbone import VGGBackbone
 
 class SSD(DetectorTemplate):
     def __init__(self, model_config, training=True):
         super(SSD, self).__init__(model_config=model_config, training=training)
+
+        if isinstance(self.backbone[0], VGGBackbone) and self.backbone[0].num_layers == 16:
+            self.backbone[0].features[16].ceil_mode = True
